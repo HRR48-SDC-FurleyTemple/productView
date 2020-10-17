58f6749d5498d0b6dc038d86acedae6135c89d2c
Javier Flores <javier.floresapodaca@gmail.com>
2020-10-16 19:20:21 -0700

    Postgres Seeding Completed

--- README.md
+++ README.md
@@ -4,3 +4,25 @@ SETUP:
 2: Seed database: npm run seed:db -> should result in 100 records inserted
 3: start server: npm start
 4: Build client pack: npm run build
+
+POSTGRES SETUP:
+DISCLAIMER - Postgres must be installed in your system for the following steps. Thank you.
+5: inside the database directory please create a file called "credentials.js"
+   inside credentials.js we will create a variable called initial and export the module.
+   initial should include your username, host, password, and port. Please see example below:
+    const initial = {
+      user: 'javier',
+      host: 'localhost',
+      password: 'cookies123',
+      port: 5432,
+    }
+
+    module.exports.initial = initial;
+6: npm run dataGenPG
+  Please wait til you see the following: finished data generation: X seconds
+7: npm run createPG
+  Please wait til you see the following messages: Database successfully created!
+  Table successfully created!
+8: npm run seedPG
+  Please wait til you see the following: finished seeding Postgres database: X seconds
+
---
+++ database/pgConnection.js
@@ -0,0 +1,65 @@
+const pgtools = require("pgtools");
+const { Client } = require('pg');
+const credentials = require('./credentials.js');
+
+var client;
+
+const createConnect = () => {
+  const finalizedCredentials = Object.assign({}, credentials.initial);
+  finalizedCredentials.database = "tenmillionrecords";
+  client = new Client(finalizedCredentials);
+
+  client.connect((err, res) => {
+    if (err) {
+      return pgtools.createdb(credentials.initial, "tenmillionrecords", function(err, res) {
+        if (err) {
+          console.log('error occurred:', err)
+          process.exit(-1);
+        }
+      }).then((res) => {
+        createConnect();
+      })
+    }
+    console.log('Database successfully created!')
+    return createTable();
+  })
+}
+
+const createTable = () => {
+  const query = `
+    CREATE TABLE IF NOT EXISTS records (
+      id SERIAL PRIMARY KEY,
+      name TEXT,
+      description TEXT,
+      articleNumber TEXT,
+      details TEXT,
+      materials TEXT,
+      sustainability TEXT,
+      width SMALLINT,
+      height SMALLINT,
+      length SMALLINT,
+      weight SMALLINT,
+      packages SMALLINT,
+      shortDesc TEXT,
+      threadCount SMALLINT,
+      pillowcaseQuantity SMALLINT,
+      duvetCoverLength SMALLINT,
+      duvetCoverWidth SMALLINT,
+      pillowcaseLength SMALLINT,
+      pillowcaseWidth SMALLINT,
+      fitting TEXT,
+      imagesUrls TEXT
+    );
+  `;
+
+  client.query(query, (err, res) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Table successfully created!')
+    client.end();
+  });
+}
+
+createConnect();
